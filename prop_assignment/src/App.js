58498ba8e81d838d85a7cc638d55f67fea680e2e
Lynn Ventura <m_lynnventura@yahoo.com>
2023-01-12 15:13:27 -0800

    first commit

---
+++ prop_assignment/src/App.js
@@ -0,0 +1,34 @@
+import './App.css';
+import PersonCard from './components/PersonCard';
+
+function App() {
+  return (
+    <div className="App">
+      <PersonCard
+        lastName = { "Doe" }
+        firstName = { "Jane" }
+        age = { 45 }
+        hairColor = { "Black"} /> 
+
+      <PersonCard
+        lastName = { "Smith" }
+        firstName = { "John" }
+        age = { 88 }
+        hairColor = { "Brown"} />
+
+      <PersonCard
+        lastName = { "Fillmore" }
+        firstName = { "Millard" }
+        age = { 50 }
+        hairColor = { "Brown"} /> 
+
+      <PersonCard
+        lastName = { "Smith" }
+        firstName = { "Marie" }
+        age = { 62 }
+        hairColor = { "Brown"} /> 
+    </div>
+  );
+}
+
+export default App;
