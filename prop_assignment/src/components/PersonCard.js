53456e7df6a729822b402fe3807b6ad9ccb3e6b3
Lynn Ventura <m_lynnventura@yahoo.com>
2023-01-16 19:26:47 -0800

    added useState hook into PersonCard.js

--- prop_assignment/src/components/PersonCard.js
+++ prop_assignment/src/components/PersonCard.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const PersonCard = (props) => {
+    const [ ageCount, setAgeCount ] = useState(props.age);
+
     return (
         <div>
-            <h2> { props.lastName }, { props.firstName} </h2>
-            <p> Age: { props.age } </p>
+            <h2> { props.lastName }, { props.firstName } </h2>
+            <p> Age: { ageCount } </p>
             <p> Hair Color: { props.hairColor } </p>
+            <button onClick ={ (event) => setAgeCount(ageCount + 1)}>Happy Birthday, {props.firstName}!</button>
         </div>
     )
 }
